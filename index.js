9b92df11c80a5d40e43f9b85a1a9145b4a470919
Nataliya R <n.rudenko@gmail.com>
2020-09-28 19:14:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,12 @@ return min;
 }
 
 function selectionSort(array){
-
+let newMin;
+let sorted = [];
+ while (array.length !== 0) {
+  newMin = findMinAndRemove(array);
+  sorted.push(newMin);
+  
+}
+return sorted;
 }
