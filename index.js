0281dfa170f73b7ef76758e1e26f58a959987a53
Nataliya R <n.rudenko@gmail.com>
2020-09-28 19:07:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function findMinAndRemove(array){
 let smallestEl = []
 for(let i = 0; i < array.length; i++){
-  if(array[i] < i)
+  if(array[i] < i){
+    smallestEl.push(array[i])
+  }
   
 }
-
+return smallestEl
 }
 
 function selectionSort(array){
