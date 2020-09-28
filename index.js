b07bf4c5085ce9fbe7a65c11dffab8f06a839c9d
Nataliya R <n.rudenko@gmail.com>
2020-09-28 19:04:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 function findMinAndRemove(array){
-let newArr = []
-let sorted = array.sort()
-let firstElement = sorted.shift()
-return newArr.push(firstElement)
+
 }
 
 function selectionSort(array){
-let sorted = array.sort()
-return sorted
+
 }
